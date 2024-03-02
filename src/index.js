1e05e166a46e4a1e43886e42cc78430804796b99
Brett Ossman <brettossman@gmail.com>
2024-03-02 15:17:00 -0500

    Section2  - Module Systems Overview

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) Get a reference to the div with id root
 const rootElement = document.getElementById('root');
@@ -8,19 +9,5 @@ const rootElement = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(rootElement);
 
-// 4) Create a component
-function App() {
-    return (
-        <div className="wrapper">
-          <textarea
-            readOnly
-            maxLength={3}
-            spellCheck
-            style={{backgroundColor: 'gray'}}
-          />
-        </div>
-      );   
-    }
-
-// 5) Show the component on the screen
+// 4) Show the component on the screen
 root.render(<App />);
